8016ae2a3b419de78e368676da062e45d6f7fae2
Manish Kumar Thakur <thakur7.manish@gmail.com>
2016-09-16 14:59:51 +0530

    Created algo for Fibonacci using dynamic programming

---
+++ Algorithms/fibonacci.c
@@ -0,0 +1,40 @@
+/* 
+ * File:   main.c
+ * Author: mthakur
+ *
+ * Created on September 16, 2016, 1:08 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+unsigned long lookUp[100];
+
+unsigned long fibonacciSumTillTerm(long n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        if (lookUp[n] != -1)
+            return lookUp[n];
+        else
+            lookUp[n - 1] = fibonacciSumTillTerm(n - 1);
+        lookUp[n - 2] = fibonacciSumTillTerm(n - 2);
+        return lookUp[n - 1] + lookUp[n - 2];
+    }
+
+}
+
+int main(int argc, char** argv) {
+
+    for (int i = 0; i < 100; i++) lookUp[i] = -1;
+    printf("%s", "Enter the term");
+    unsigned long n;
+    scanf("%lu", &n);
+    long sum = fibonacciSumTillTerm(n);
+    printf("%lu", sum);
+    return (EXIT_SUCCESS);
+}
+
